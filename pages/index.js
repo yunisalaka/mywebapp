e308e3011809bbe634b5d8ef7e5a22e124d5b61a
Yunis Alaka <yunisalaka@Yuniss-MacBook-Pro.local>
2022-08-09 15:28:28 +0100

    world

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello
+          Hello-World
         </h1>
 
         <p className={styles.description}>
